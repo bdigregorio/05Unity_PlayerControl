0b23ea879e72acebd78e10fb82ec55632eb384ed
Benjamin DiGregorio <ben@digregor.io>
2020-12-21 23:07:02 -0800

    Create FollowPlayer script for main camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // We'll move the vehicle forward here
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // multiplying * Time.deltaTime converts the translation
+        // to per second from per frame
+        transform.Translate( speed * Time.deltaTime * Vector3.forward);
     }
 }
